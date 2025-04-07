d4a3d48a9addfb4161e845a0072698074cfe1d7e
dronesito <dronesitodron@gmail.com>
2025-04-07 13:15:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,24 +9,29 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
 
 
 
-
-    // Получаем модальное окно
-    var modal = document.getElementById("contact-modal");
-    // Получаем кнопку, которая открывает модальное окно
-    var btn = document.querySelector(".help button");
-    // Получаем элемент <span>, который закрывает модальное окно
-    var span = document.getElementsByClassName("close")[0];
-    // Когда пользователь нажимает на кнопку, открывается модальное окно
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-    // Когда пользователь нажимает на <span> (x), закрывается модальное окно
-    span.onclick = function() {
+// Получаем модальное окно
+var modal = document.getElementById("contact-modal");
+// Получаем кнопку, которая открывает модальное окно
+var btn = document.getElementById("open-modal");
+// Получаем элемент <span>, который закрывает модальное окно
+var span = document.getElementsByClassName("close")[0];
+// Когда пользователь нажимает на кнопку, открывается модальное окно
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+// Когда пользователь нажимает на <span> (x), закрывается модальное окно
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// Когда пользователь нажимает в любом месте вне модального окна, оно закрывается
+window.onclick = function(event) {
+    if (event.target == modal) {
         modal.style.display = "none";
     }
-    // Когда пользователь нажимает в любом месте вне модального окна, оно закрывается
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+}
+// Обработчик для кнопки "Отправить"
+document.getElementById("submit-button").onclick = function() {
+    // Здесь можно добавить логику для обработки отправки формы
+    alert("Форма отправлена!");
+    modal.style.display = "none"; // Закрываем модальное окно после отправки
+}
